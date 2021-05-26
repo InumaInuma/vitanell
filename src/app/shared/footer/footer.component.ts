61b54f1518803de0a47c57d4c026224fb184c7a9
YOGA <Anthony_Inuma@hotmail.com>
2021-05-26 11:21:17 -0500

    cuarto

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -9,7 +9,7 @@ export class FooterComponent implements OnInit {
 
   constructor() { }
 
-  public phone = "+51931610069"
+  public phone = "+51936104581"
   public title = "Hola deseo informacion de los productos de vitanell";
 
   ngOnInit(): void {
